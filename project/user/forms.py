f5a72af71888b9a87368fc09070331b80db57179
myejin <rlagpwlsq78@gmail.com>
2021-06-29 17:48:03 +0000

    deprecation warning 해결
    count() 대신 count_documents(~) 사용

--- project/user/forms.py
+++ project/user/forms.py
@@ -5,7 +5,7 @@ import re
 from ..model import conn
 
 def Signup_Valid_Id(form, field):
-    cnt = conn.db.user.find({'id':field.data}).count()
+    cnt = conn.db.user.count_documents({'id':field.data})
     conn.close()
     if cnt:
         raise ValidationError('이미 존재하는 ID입니다.')
@@ -16,17 +16,17 @@ def Signup_Valid_Password(form, field):
         raise ValidationError('영문자, 숫자, 특수문자 포함해야합니다.')
 
 class Signup_Form(FlaskForm):
-    userid = StringField('userid', [InputRequired(message = '값을 입력하세요.'), Length(min = 7, max = 15, message = 'ID 길이를 맞춰주세요.'), Signup_Valid_Id])
+    userid = StringField('userid', [InputRequired(message = '값을 입력하세요.'), Length(min = 7, max = 15, message = 'Please match the ID length.'), Signup_Valid_Id])
     password = PasswordField('password', [InputRequired(message = '값을 입력하세요.'), Length(min = 7, max = 15, message = 'PW 길이를 맞춰주세요.'), Signup_Valid_Password])
     re_pw = PasswordField('re-pw', [InputRequired(message = '값을 입력하세요.'), EqualTo('password', message = '비밀번호가 일치하지 않습니다!')])
 
 
 def Login_Valid(form, field):
+    if not conn.db.user.count_documents({'id':field.data}):
+        conn.close()
+        raise ValidationError('존재하지 않는 ID입니다.')
     data = conn.db.user.find({'id':field.data})
     conn.close()
-    if not data.count():
-        raise ValidationError('존재하지 않는 ID입니다.')
-
     if data[0]['pw'] != form['password'].data:
         raise ValidationError('비밀번호가 일치하지 않습니다.')
 
