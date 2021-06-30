365a0e48c3223c24ec97018d2a3735dd7afede30
myejin <rlagpwlsq78@gmail.com>
2021-06-30 07:46:53 +0000

    에러메세지 영문으로 변경
    한글에서 영문으로 변경

--- project/user/forms.py
+++ project/user/forms.py
@@ -8,17 +8,17 @@ def Signup_Valid_Id(form, field):
     cnt = conn.db.user.count_documents({'id':field.data})
     conn.close()
     if cnt:
-        raise ValidationError('이미 존재하는 ID입니다.')
+        raise ValidationError('The ID already exists.')
 
 def Signup_Valid_Password(form, field):
     chk = re.compile(r'(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[^\w\s]).*')
     if not chk.search(field.data):
-        raise ValidationError('영문자, 숫자, 특수문자 포함해야합니다.')
+        raise ValidationError('Must contain alphabetic, numeric, and special characters.')
 
 class Signup_Form(FlaskForm):
-    userid = StringField('userid', [InputRequired(message = '값을 입력하세요.'), Length(min = 7, max = 15, message = 'Please match the ID length.'), Signup_Valid_Id])
-    password = PasswordField('password', [InputRequired(message = '값을 입력하세요.'), Length(min = 7, max = 15, message = 'PW 길이를 맞춰주세요.'), Signup_Valid_Password])
-    re_pw = PasswordField('re-pw', [InputRequired(message = '값을 입력하세요.'), EqualTo('password', message = '비밀번호가 일치하지 않습니다!')])
+    userid = StringField('userid', [InputRequired(message = 'blank'), Length(min = 7, max = 15, message = 'Please match the ID length.'), Signup_Valid_Id])
+    password = PasswordField('password', [InputRequired(message = 'blank'), Length(min = 7, max = 15, message = 'Please match the Password length.'), Signup_Valid_Password])
+    re_pw = PasswordField('re-pw', [InputRequired(message = 'blank'), EqualTo('password', message = 'The password does not match.')])
 
 
 def Login_Valid(form, field):
