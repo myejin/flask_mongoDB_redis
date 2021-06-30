78aa184ef503d39ba78f408b5f9d609b1c023a9f
myejin <rlagpwlsq78@gmail.com>
2021-06-30 07:53:50 +0000

    회원가입 API 테스트용
    회원가입 페이지 접속, 회원가입(POST) 성공, 회원가입 실패 조건 추가

--- tests/functional/test_views.py
+++ tests/functional/test_views.py
@@ -1,6 +1,25 @@
-import re
+def test_signup_page(test_client, test_db):
+    # 회원가입 페이지 접속
+    resp = test_client.get('/signup/')
+    assert resp.status_code == 200
+
+def test_valid_signup(test_client, test_db):
+    # 회원가입 성공 
+    resp = test_client.post('/signup/', data = dict(
+        userid = 'test002',
+        password = 'test002!',
+        re_pw = 'test002!'
+    ),
+    follow_redirects = True)
+    assert resp.status_code == 200
+    assert b'Please match the ID length.' not in resp.data
+    assert b'The ID already exists.' not in resp.data
+    assert b'Please match the Password length.' not in resp.data
+    assert b'Must contain alphabetic, numeric, and special characters.' not in resp.data
+    assert b'The password does not match.' not in resp.data
 
 def test_invalid_signup(test_client, test_db):
+    # 1. ID 길이 
     resp = test_client.post('/signup/', data = dict(
         userid = 'test1',
         password = 'test002!',
@@ -9,3 +28,38 @@ def test_invalid_signup(test_client, test_db):
     follow_redirects = True)
     assert b'Please match the ID length.' in resp.data
     
+    # 2. 이미 존재하는 ID 
+    resp = test_client.post('/signup/', data = dict(
+        userid = 'test001',
+        password = 'test001!',
+        re_pw = 'test001!'
+    ),
+    follow_redirects = True)
+    assert b'The ID already exists.' in resp.data
+
+    # 3. PW 길이
+    resp = test_client.post('/signup/', data = dict(
+        userid = 'test002',
+        password = 'test2!',
+        re_pw = 'test2!'
+    ),
+    follow_redirects = True)
+    assert b'Please match the Password length.' in resp.data
+
+    # 4. 비밀번호 영문자, 숫자, 특수문자 미포함
+    resp = test_client.post('/signup/', data = dict(
+        userid = 'test002',
+        password = 'test002',
+        re_pw = 'test002'
+    ),
+    follow_redirects = True)
+    assert b'Must contain alphabetic, numeric, and special characters.' in resp.data
+
+    # 5. 비밀번호 확인 불일치 
+    resp = test_client.post('/signup/', data = dict(
+        userid = 'test002',
+        password = 'test002!',
+        re_pw = 'test002'
+    ),
+    follow_redirects = True)
+    assert b'The password does not match.' in resp.data
