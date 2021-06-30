b363d75cc124876480659a3ce160371f18dae72b
Ubuntu <ubuntu@ip-172-31-4-100.ap-northeast-2.compute.internal>
2021-06-30 11:34:31 +0000

    extension() 수정
    pymongo에서 flask_pymongo로 모듈 변경, 전역범위에 mongo 생성 후 extensions 함수에 추가

--- project/__init__.py
+++ project/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_session import Session
+from flask_pymongo import PyMongo
 
 # flask extension 전역범위에 생성 
 server_session = Session()
+mongo = PyMongo()
 
 def create_app(config_file = None):
     app = Flask(__name__, instance_relative_config = True) #?
@@ -13,6 +15,7 @@ def create_app(config_file = None):
 
 def extensions(app):
     server_session.init_app(app)
+    mongo.init_app(app)
 
 def blueprints(app):
     from .user import user_bp
