e7f84048abac781b9b678c107a530ee8b8352235
Ubuntu <ubuntu@ip-172-31-4-100.ap-northeast-2.compute.internal>
2021-06-30 11:56:42 +0000

    회원가입, 로그인 테스트코드 수정
    현재 추가된 함수 모두 디버깅 완료

--- tests/functional/test_views.py
+++ tests/functional/test_views.py
@@ -3,23 +3,6 @@ def test_signup_page(test_client):
     resp = test_client.get('/signup/')
     assert resp.status_code == 200
 
-def test_valid_signup(test_client, test_db):
-    # 회원가입 성공 
-    resp = test_client.post('/signup/', data = dict(
-        userid = 'test002',
-        password = 'test002!',
-        re_pw = 'test002!'
-    ),
-    follow_redirects = True)
-    assert resp.status_code == 200
-    #assert b'Signed up successfully.' in resp.data 
-    assert b"The CSRF token is missing." not in resp.data
-    assert b'Please match the ID length.' not in resp.data
-    assert b'The ID already exists.' not in resp.data
-    assert b'Please match the Password length.' not in resp.data
-    assert b'Must contain alphabetic, numeric, and special characters.' not in resp.data
-    assert b'The password does not match.' not in resp.data
-
 def test_invalid_signup(test_client, test_db):
     # 1. ID 길이 
     resp = test_client.post('/signup/', data = dict(
@@ -65,8 +48,24 @@ def test_invalid_signup(test_client, test_db):
     ),
     follow_redirects = True)
     assert b'The password does not match.' in resp.data
-
     assert b"The CSRF token is missing." not in resp.data
+
+def test_valid_signup(test_client, test_db):
+    # 회원가입 성공 
+    resp = test_client.post('/signup/', data = dict(
+        userid = 'test002',
+        password = 'test002!',
+        re_pw = 'test002!'
+    ),
+    follow_redirects = True)
+    assert resp.status_code == 200
+    assert b'Signed up successfully.' in resp.data
+    assert b"The CSRF token is missing." not in resp.data
+    assert b'Please match the ID length.' not in resp.data
+    assert b'The ID already exists.' not in resp.data
+    assert b'Please match the Password length.' not in resp.data
+    assert b'Must contain alphabetic, numeric, and special characters.' not in resp.data
+    assert b'The password does not match.' not in resp.data
 
 
 def test_login_page(test_client):
@@ -74,23 +73,11 @@ def test_login_page(test_client):
     resp = test_client.get('/login/')
     assert resp.status_code == 200
 
-def test_valid_login(test_client, test_db):
-    # 로그인 성공 
-    resp = test_client.post('/login/', data = dict(
-        userid = 'test001',
-        password = 'test001!',
-        re_pw = 'test001!'
-    ),
-    follow_redirects = True)
-    assert resp.status_code == 200
-    assert b'The ID does not exist.' not in resp.data
-    assert b'The password does not match.' not in resp.data
-
 def test_invalid_login(test_client, test_db):
     # 1. 존재하지 않는 ID 
     resp = test_client.post('/login/', data = dict(
-        userid = 'test002',
-        password = 'test002!'
+        userid = 'test003',
+        password = 'test003!'
     ),
     follow_redirects = True)
     assert b'The ID does not exist.' in resp.data
@@ -102,5 +89,16 @@ def test_invalid_login(test_client, test_db):
     ),
     follow_redirects = True)
     assert b'The password does not match.' in resp.data
-
     assert b"The CSRF token is missing." not in resp.data
+
+def test_valid_login(test_client, test_db):
+    # 로그인 성공 
+    resp = test_client.post('/login/', data = dict(
+        userid = 'test001',
+        password = 'test001!'
+    ),
+    follow_redirects = True)
+    assert resp.status_code == 200
+    assert b'The ID does not exist.' not in resp.data
+    assert b"The CSRF token is missing." not in resp.data
+    assert b'The password does not match.' not in resp.data
