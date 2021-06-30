d9545a539340b4fc2646415276db4c167dbfefee
myejin <rlagpwlsq78@gmail.com>
2021-06-30 08:52:16 +0000

    로그인 테스트코드 추가
    코드에 에러 있음. 수정해야함.

--- tests/functional/test_views.py
+++ tests/functional/test_views.py
@@ -1,4 +1,4 @@
-def test_signup_page(test_client, test_db):
+def test_signup_page(test_client):
     # 회원가입 페이지 접속
     resp = test_client.get('/signup/')
     assert resp.status_code == 200
@@ -12,6 +12,8 @@ def test_valid_signup(test_client, test_db):
     ),
     follow_redirects = True)
     assert resp.status_code == 200
+    #assert b'Signed up successfully.' in resp.data 
+    assert b"The CSRF token is missing." not in resp.data
     assert b'Please match the ID length.' not in resp.data
     assert b'The ID already exists.' not in resp.data
     assert b'Please match the Password length.' not in resp.data
@@ -63,3 +65,42 @@ def test_invalid_signup(test_client, test_db):
     ),
     follow_redirects = True)
     assert b'The password does not match.' in resp.data
+
+    assert b"The CSRF token is missing." not in resp.data
+
+
+def test_login_page(test_client):
+    # 로그인 페이지 접속
+    resp = test_client.get('/login/')
+    assert resp.status_code == 200
+
+def test_valid_login(test_client, test_db):
+    # 로그인 성공 
+    resp = test_client.post('/login/', data = dict(
+        userid = 'test001',
+        password = 'test001!',
+        re_pw = 'test001!'
+    ),
+    follow_redirects = True)
+    assert resp.status_code == 200
+    assert b'The ID does not exist.' not in resp.data
+    assert b'The password does not match.' not in resp.data
+
+def test_invalid_login(test_client, test_db):
+    # 1. 존재하지 않는 ID 
+    resp = test_client.post('/login/', data = dict(
+        userid = 'test002',
+        password = 'test002!'
+    ),
+    follow_redirects = True)
+    assert b'The ID does not exist.' in resp.data
+    
+    # 2. 비밀번호 불일치
+    resp = test_client.post('/login/', data = dict(
+        userid = 'test001',
+        password = 'test001'
+    ),
+    follow_redirects = True)
+    assert b'The password does not match.' in resp.data
+
+    assert b"The CSRF token is missing." not in resp.data
