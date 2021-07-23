aebdbcb3e4ebf74e6f07be4cbe3df41c0e4cc4c5
Ubuntu <ubuntu@ip-172-31-2-28.ap-northeast-2.compute.internal>
2021-07-23 04:20:27 +0000

    에러핸들러 추가
    애플리케이션 팩토리에 handle_error 함수 호출

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_session import Session
 from flask_pymongo import PyMongo
 
@@ -11,6 +11,7 @@ def create_app(config_file = None):
     app.config.from_pyfile(config_file)
     extensions(app)
     blueprints(app)
+    handle_error(app)
     return app
 
 def extensions(app):
@@ -20,3 +21,8 @@ def extensions(app):
 def blueprints(app):
     from .user import user_bp
     app.register_blueprint(user_bp)
+
+def handle_error(app):
+    @app.errorhandler(Exception)
+    def err_handler(e):
+        return jsonify({'error': str(e), 'data': None})
